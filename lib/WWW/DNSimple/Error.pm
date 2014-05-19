a25cb0afefaa1e3fe2ee4dc4f9ceaeef8df965e9
James Aitken <loonypandora@gmail.com>
2014-05-19 19:10:52 +0100

    Implement proper throwable errors

---
+++ lib/WWW/DNSimple/Error.pm
@@ -0,0 +1,49 @@
+package WWW::DNSimple::Error;
+
+# ABSTRACT: Error handling for DNSimple API.
+
+=head1 NAME
+
+WWW::DNSimple::Error - Error handling for DNSimple API.
+
+=head1 SYNOPSIS
+
+=cut
+
+use strict;
+use Moo;
+
+extends "Throwable::Error";
+
+
+1;
+
+
+=head1 MORE INFORMATION
+
+=head2 Purpose
+
+=head2 Rationale
+
+=head2 Configuration
+
+
+=head1 KNOWN ISSUES
+
+
+=head1 SEE ALSO
+
+
+=head1 AUTHOR
+
+James Aitken <jaitken@cpan.org>
+
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by James Aitken.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
