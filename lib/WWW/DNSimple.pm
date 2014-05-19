0aff5e961cc18b4e58f4e59a4a256b2b75b4c58e
James Aitken <loonypandora@gmail.com>
2014-05-19 01:58:57 +0100

    Real first commit

---
+++ lib/WWW/DNSimple.pm
@@ -0,0 +1,52 @@
+package WWW::DNSimple
+
+# ABSTRACT: Perl interface to the dnsimple.com API
+
+=head1 NAME
+
+WWW::DNSimple - Perl interface to the dnsimple.com API
+
+=head1 SYNOPSIS
+
+=cut
+
+use strict;
+
+our $VERSION = '0.0.1';
+
+
+
+
+
+
+1;
+
+
+=head1 MORE INFORMATION
+
+=head2 Purpose
+
+=head2 Rationale
+
+=head2 Configuration
+
+
+=head1 KNOWN ISSUES
+
+
+=head1 SEE ALSO
+
+
+=head1 AUTHOR
+
+James Aitken <jaitken@cpan.org>
+
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by James Aitken.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
